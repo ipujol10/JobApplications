b3a0679377e709ee7562a28d52781dc64e9fbde0
Iñaki Pujol <ipujol10@gmail.com>
2023-11-22 13:07:23 +0100

    UPDATE: function to print the list it takes the pointer to the head and recursively it prints all nodes until it reaches the tail

--- C/Signaloid/main.c
+++ C/Signaloid/main.c
@@ -1,11 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 typedef struct Node {
     int data;
     struct Node* next;
 } Node;
 
+typedef Node LinkedList;
+
+void printList(LinkedList* list);
+
 int main(int argc, char *argv[]) {
 
-    Node node;
+    Node n0, n1;
+    n1 = (Node) {1, NULL};
+    n0 = (Node) {0, &n1};
+    LinkedList list = n0;
+    printList(&list);
 
     return 0;
+}
+
+void printList(LinkedList* list) {
+    if (list->next == NULL) {
+        printf("%i\n", list->data);
+        return;
+    }
+    printf("%i -> ", list->data);
+    printList(list->next);
 }
