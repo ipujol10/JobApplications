3a53413116f4f177df93384309d158e50d3d6b7b
Iñaki Pujol <ipujol10@gmail.com>
2023-11-22 14:54:35 +0100

    UPDATE: insert a value in the specified position in the linked list if the index is out of range it prints a statement and keeps the list     intact

--- C/Signaloid/main.c
+++ C/Signaloid/main.c
@@ -8,23 +8,60 @@ typedef struct Node {
 
 typedef Node* LinkedList;
 
-void printList(LinkedList* list);
+void printList(LinkedList list);
 void insert(LinkedList* list, int value, int position);
 
 int main(int argc, char *argv[]) {
     printf("Start with a single node with value 1\n");
     Node head = {1, NULL};
     LinkedList list = &head;
-    printList(&list);
+    printList(list);
+
+    printf("\nAdd the node 3 at the end and the node 0 at the beginnig\n");
+    insert(&list, 3, 1);
+    insert(&list, 0, 0);
+    printList(list);
+
+    printf("\nAdd the node 2 between 1 and 3\n");
+    insert(&list, 2, 2);
+    printList(list);
+
+    printf("\nTry to add the node 5 at position 5. It prints an error and keeps"
+            " the list intact\n");
+    insert(&list, 5, 5);
+    printList(list);
 
     return 0;
 }
 
-void printList(LinkedList* list) {
-    Node node = **list;
+void printList(LinkedList list) {
+    Node node = *list;
     while (node.next != NULL) {
         printf("%i -> ", node.data);
         node = *node.next;
     };
     printf("%i\n", node.data);
+}
+
+void insert(LinkedList* list, int value, int position) {
+    if (position == 0) {
+        Node* ptr = malloc(sizeof(Node));
+        *ptr = (Node) {value, *list};
+        *list = ptr;
+        return;
+    }
+
+    Node* previous = *list;
+    int count = 1;
+    while (count < position) {
+        if (previous->next == NULL) {
+            printf("Index out of range\n");
+            return;
+        }
+        previous = previous->next;
+        count++;
+    }
+    Node* ptr = malloc(sizeof(Node));
+    *ptr = (Node) {value, previous->next};
+    previous->next = ptr;
 }
